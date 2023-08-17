5fb3d48ab3c820c7b078c0b3d304c2fee540dd8c
Sanjeev <sanjeevkansyakar789@gmail.com>
2023-08-17 21:48:22 +0530

    handle import

--- components/index.ts
+++ components/index.ts
@@ -7,7 +7,7 @@ import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
 import CarCard from "./CarCard";
 import CarDetails from "./CarDetails";
-import ShowMore from "./showMore";
+import ShowMore from "./ShowMore";
 
 export {
   Hero,
